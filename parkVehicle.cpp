81c14ec7e031a7c85eeecafba770a4d006554d21
Akhilesh Panigrahi <akhileshpanigrahi@Akhileshs-MacBook-Air.local>
2025-11-16 17:27:47 +0530

    Implement logic for parking vehicles

---
+++ parkVehicle.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <algorithm>
+#include "parkVehicle.h"
+using namespace std;
+void parkVehicle(vector<Vehicle>&v,int &cc,int&cb,int&tv,int&tc,int&tb,int MC,int MB){
+ string t,no; 
+ cout<<"Enter vehicle type (Car/Bike): "; cin>>t;
+ transform(t.begin(),t.end(),t.begin(),::toupper);
+ if(t!="CAR"&&t!="BIKE"){cout<<"Invalid.\n";return;}
+ if(t=="CAR"&&cc>=MC){cout<<"Car full.\n";return;}
+ if(t=="BIKE"&&cb>=MB){cout<<"Bike full.\n";return;}
+ cout<<"Enter vehicle number: "; cin>>no;
+ v.emplace_back(no,t,time(0));
+ if(t=="CAR"){cc++;tc++;} else {cb++;tb++;}
+ tv++;
+ cout<<"Vehicle parked.\n";
+}
